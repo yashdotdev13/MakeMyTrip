57e31ad46c77867b72e97d483e81ac38881c6277
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-01 00:22:22 +0530

    created the refresh_tokens entity

---
+++ Auth-service/src/main/java/com/company/MakeMyTrip/Auth_service/enums/RefreshToken.java
@@ -0,0 +1,32 @@
+package com.company.MakeMyTrip.Auth_service.enums;
+
+
+import com.company.MakeMyTrip.Auth_service.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+}
