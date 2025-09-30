49f50d6e8b92214b9ba425f21a8bbff83752cdb2
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-01 01:25:01 +0530

    created the Auth service interface

---
+++ Auth-service/src/main/java/com/company/MakeMyTrip/Auth_service/dtos/RegisterResponse.java
@@ -0,0 +1,12 @@
+package com.company.MakeMyTrip.Auth_service.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterResponse {
+}
---
+++ Auth-service/src/main/java/com/company/MakeMyTrip/Auth_service/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.company.MakeMyTrip.Auth_service.service;
+
+import com.company.MakeMyTrip.Auth_service.dtos.*;
+
+public interface AuthService {
+
+    RegisterResponse register(RegisterRequest registerRequest);
+
+    AuthResponse login(LoginRequest loginRequest);
+
+    AuthResponse refreshToken(RefreshTokenRequest refreshTokenRequest);
+}
