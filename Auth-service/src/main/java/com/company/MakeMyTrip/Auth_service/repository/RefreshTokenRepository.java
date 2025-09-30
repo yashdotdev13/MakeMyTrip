6ec497b3927ffa34d83461ecd31d10141eec6fec
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-01 00:39:44 +0530

    created the user repository and RefresgTokenrepositroy

---
+++ Auth-service/src/main/java/com/company/MakeMyTrip/Auth_service/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.company.MakeMyTrip.Auth_service.repository;
+
+import com.company.MakeMyTrip.Auth_service.entity.RefreshToken;
+import com.company.MakeMyTrip.Auth_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUser(User user);
+}
---
+++ Auth-service/src/main/java/com/company/MakeMyTrip/Auth_service/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.company.MakeMyTrip.Auth_service.repository;
+
+import com.company.MakeMyTrip.Auth_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
