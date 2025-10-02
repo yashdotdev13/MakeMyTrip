67d78f5d27d3e7734023466f4c25ae8cf70b7648
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-02 09:31:02 +0530

    added the userProfileService

---
+++ userProfile-service/src/main/java/com/company/MakeMyTrip/userProfile_service/UserProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.MakeMyTrip.userProfile_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileServiceApplication.class, args);
+	}
+
+}
---
+++ userProfile-service/src/test/java/com/company/MakeMyTrip/userProfile_service/UserProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.MakeMyTrip.userProfile_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
