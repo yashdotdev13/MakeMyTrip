19aaa958895aec2df8a8e4aa20680a704465b9e7
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-02 22:24:20 +0530

    created the exceptions in booking-service

---
+++ booking-service/src/main/java/com/company/MakeMyTrip/booking_service/exceptions/RuntimeConflictException.java
@@ -0,0 +1,11 @@
+package com.company.MakeMyTrip.booking_service.exceptions;
+
+public class RuntimeConflictException extends RuntimeException{
+    public RuntimeConflictException() {
+    }
+
+    public RuntimeConflictException(String message) {
+        super(message);
+    }
+}
+
