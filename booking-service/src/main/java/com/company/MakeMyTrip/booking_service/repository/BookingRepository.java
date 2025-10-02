05da87e95f2868ee6eb0c564f40d9a623ff9f4c0
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-02 23:56:52 +0530

    implemented the bookingRepository

---
+++ booking-service/src/main/java/com/company/MakeMyTrip/booking_service/repository/BookingRepository.java
@@ -0,0 +1,17 @@
+package com.company.MakeMyTrip.booking_service.repository;
+
+import com.company.MakeMyTrip.booking_service.entity.Booking;
+import com.company.MakeMyTrip.booking_service.enums.BookingType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    // find all bookings for a given user
+    List<Booking> findByUserId(Long userId);
+
+    // Optional: Find bookings by type for a user
+    List<Booking> findByUserIdAndBookingType(Long userId, BookingType bookingType);
+
+}
