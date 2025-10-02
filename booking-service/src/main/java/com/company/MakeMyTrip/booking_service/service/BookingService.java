f4d38a89939004b9ea007cc72ce2679133eed036
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-03 00:14:18 +0530

    created the booking service interface in the booking service

---
+++ booking-service/src/main/java/com/company/MakeMyTrip/booking_service/service/BookingService.java
@@ -0,0 +1,19 @@
+package com.company.MakeMyTrip.booking_service.service;
+
+import com.company.MakeMyTrip.booking_service.dtos.BookingRequest;
+import com.company.MakeMyTrip.booking_service.dtos.BookingResponse;
+
+import java.util.List;
+
+public interface BookingService {
+
+    BookingResponse createBooking(BookingRequest request);
+
+    BookingResponse getBookingById(Long bookingId);
+
+    List<BookingResponse> getBookingByUser();
+
+    BookingResponse updateBooking(Long bookingId, BookingRequest request);
+
+    void cancelBooking(Long bookingId);
+}
