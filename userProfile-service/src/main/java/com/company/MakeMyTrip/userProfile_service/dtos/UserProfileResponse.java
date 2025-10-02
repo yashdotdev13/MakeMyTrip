3638c9a65f4d24bfcc872b449d34df0ae004555b
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-02 11:58:55 +0530

    user-profile-service is working fine

--- userProfile-service/src/main/java/com/company/MakeMyTrip/userProfile_service/dtos/UserProfileResponse.java
+++ userProfile-service/src/main/java/com/company/MakeMyTrip/userProfile_service/dtos/UserProfileResponse.java
@@ -3,9 +3,11 @@ package com.company.MakeMyTrip.userProfile_service.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserProfileResponse {
 
     private Long id;
