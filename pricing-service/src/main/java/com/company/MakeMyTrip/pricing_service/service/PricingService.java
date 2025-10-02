485f7b5c6cd8f2d894e613af84802abc9d94cb45
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-03 03:06:54 +0530

    created the service interface for Pricing service

--- pricing-service/src/main/java/com/company/MakeMyTrip/pricing_service/service/PricingService.java
+++ pricing-service/src/main/java/com/company/MakeMyTrip/pricing_service/service/PricingService.java
@@ -1,5 +1,23 @@
 package com.company.MakeMyTrip.pricing_service.service;
 
+import com.company.MakeMyTrip.pricing_service.dtos.PriceLockResponse;
+import com.company.MakeMyTrip.pricing_service.dtos.PriceQuoteResponse;
+
+import java.util.List;
+
 public interface PricingService {
 
+
+    PriceQuoteResponse getPriceQuote(Long referenceId,
+                                     String bookingType,
+                                     Long userId,
+                                     int quantity, String travelDate);
+
+
+    PriceLockResponse lockPrice(Long referenceId,
+                                String bookingType, Long userId);
+
+    boolean releasePriceLock(Long lockId);
+
+    List<String> getAllPriceRules();
 }
