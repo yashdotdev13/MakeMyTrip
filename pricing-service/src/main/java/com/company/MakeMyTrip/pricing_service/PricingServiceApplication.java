1649a5feec5fe0bd4d637610057ee0d56be1d153
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-05 11:41:28 +0530

    configured the feign client in the pricing=service

--- pricing-service/src/main/java/com/company/MakeMyTrip/pricing_service/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/company/MakeMyTrip/pricing_service/PricingServiceApplication.java
@@ -2,8 +2,10 @@ package com.company.MakeMyTrip.pricing_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PricingServiceApplication {
 
 	public static void main(String[] args) {
