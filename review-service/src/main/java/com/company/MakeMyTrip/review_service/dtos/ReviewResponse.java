0d0762f2c532fd46a1f8149419aebec9c5adb439
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-07 17:21:10 +0530

    added the required dtos

---
+++ review-service/src/main/java/com/company/MakeMyTrip/review_service/dtos/ReviewResponse.java
@@ -0,0 +1,25 @@
+package com.company.MakeMyTrip.review_service.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewResponse {
+
+    private Long id;
+    private Long userId;
+    private Long bookingId;
+    private Integer rating;
+    private String title;
+    private String comment;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ review-service/src/main/java/com/company/MakeMyTrip/review_service/dtos/ReviewSummaryResponse.java
@@ -0,0 +1,18 @@
+package com.company.MakeMyTrip.review_service.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewSummaryResponse {
+
+    private Long bookingId;
+    private Double averageRating;
+    private Long totalReviews;
+}
