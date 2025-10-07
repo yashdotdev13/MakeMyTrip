b6d46762b45ea678ba62ab1b57ff15414598894c
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-07 11:13:49 +0530

    created the paymentRepository

---
+++ payment-service/src/main/java/com/company/MakeMyTrip/payment_service/repository/PaymentRepository.java
@@ -0,0 +1,13 @@
+package com.company.MakeMyTrip.payment_service.repository;
+
+import com.company.MakeMyTrip.payment_service.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment,Long> {
+
+    Optional<Payment>  findByBookingId(Long bookingId);
+
+    Optional<Payment> findByTransactionId(String transactionId);
+}
