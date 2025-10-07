3b698055ad334d0a5a9993a5642995fa7bccd999
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-07 16:59:43 +0530

    created the review entity

---
+++ review-service/src/main/java/com/company/MakeMyTrip/review_service/entity/Review.java
@@ -0,0 +1,42 @@
+package com.company.MakeMyTrip.review_service.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "reviews")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private Long bookingId;
+
+    private Integer rating;
+    private String title;
+    private String comment;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
