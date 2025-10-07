b70be0db49684d60f3295d2d100d26af6061745d
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-07 16:53:56 +0530

    added the configuration in the review-service

---
+++ review-service/src/main/java/com/company/MakeMyTrip/review_service/auth/FeignClientInterceptor.java
@@ -0,0 +1,23 @@
+package com.company.MakeMyTrip.review_service.auth;
+
+
+
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        Long userId= UserContextHolder.getCurrentUserId();
+
+        if(userId!=null){
+            requestTemplate.header("X-User-Id",userId.toString());
+        }
+
+    }
+}
