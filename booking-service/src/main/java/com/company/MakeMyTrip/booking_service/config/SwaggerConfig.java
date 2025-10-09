27036ccbfa00ace8a84b38a657fd6d79a2eadb30
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-09 12:11:49 +0530

    added the swaggger-ui configuration in the booking-service

---
+++ booking-service/src/main/java/com/company/MakeMyTrip/booking_service/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.company.MakeMyTrip.booking_service.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI userServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Service API")
+                        .description("API documentation for the User microservice")
+                        .version("1.0.0"));
+    }
+}
