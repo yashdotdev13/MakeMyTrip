0808d28db9b0c6d1966ddecdb792d27b769436dd
98001yash <yashchauhan.gaya@gmail.com>
2026-09-19 22:34:27 +0530

    updated the dtos

--- Auth-service/src/main/java/com/company/MakeMyTrip/Auth_service/dtos/LoginRequest.java
+++ Auth-service/src/main/java/com/company/MakeMyTrip/Auth_service/dtos/LoginRequest.java
@@ -1,10 +1,17 @@
 package com.company.MakeMyTrip.Auth_service.dtos;
 
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+
+    @NotBlank(message = "Username or email is required")
+    @Size(max = 255, message = "Username or email is too long")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 1, max = 100, message = "Invalid password")
     private String password;
 }
